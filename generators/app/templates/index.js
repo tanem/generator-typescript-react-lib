5c4f2177c4120ec3d4ab41efd234e818ebcea240
Tane Morgan <tane.morgan@gmail.com>
2019-06-03 15:16:44 +1200

    Fix a couple glitches noticed during usage

--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cjs/<%= packageName %>.production.min.js')
+  module.exports = require('./<%= packageName %>.cjs.production.js')
 } else {
-  module.exports = require('./cjs/<%= packageName %>.development.js')
+  module.exports = require('./<%= packageName %>.cjs.development.js')
 }
