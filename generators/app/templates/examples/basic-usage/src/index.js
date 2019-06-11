a840702c0d3d17558a27ca6f690ee5eae8fb451f
Tane Morgan <tane.morgan@gmail.com>
2019-06-12 05:50:59 +1200

    Adjust example template

--- generators/app/templates/examples/basic-usage/src/index.js
+++ generators/app/templates/examples/basic-usage/src/index.js
@@ -1,5 +1,5 @@
+import C from '<%= packageName %>'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactSVG from 'react-svg'
 
-ReactDOM.render(<ReactSVG src="svg.svg" />, document.getElementById('root'))
+ReactDOM.render(<C />, document.getElementById('root'))
