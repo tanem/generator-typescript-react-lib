38031f8f4ca2695f0d53ee71c3154331e14c5301
Tane Morgan <464864+tanem@users.noreply.github.com>
2019-08-25 10:35:43 +1200

    Remove test folder from git

--- your-new-react-library/scripts/jest/setupJest.ts
+++
@@ -1,9 +0,0 @@
-import { createPrettyHtmlMatchers } from 'jest-prettyhtml-matchers'
-
-expect.extend(
-  createPrettyHtmlMatchers({
-    singleQuote: true,
-    sortAttributes: true,
-    wrapAttributes: true
-  })
-)
